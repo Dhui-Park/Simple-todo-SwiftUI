2f8cad73730a7b1544941f61a3a43ccd78d36625
dhuiPark <hasong925@gmail.com>
2023-09-21 21:12:41 +0900

    Initial Commit

---
+++ todo-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todo-SwiftUI
+//
+//  Created by dhui on 2023/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todo-SwiftUI/todo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_SwiftUIApp.swift
+//  todo-SwiftUI
+//
+//  Created by dhui on 2023/09/21.
+//
+
+import SwiftUI
+
+@main
+struct todo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
